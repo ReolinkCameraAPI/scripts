cacf6a5acf844010c6ff1b3596ed3858f55a1ec0
Helias <stefanoborzi32@gmail.com>
2022-09-11 20:23:45 +0200

    feat: add flags to download.sh, improve README

--- download.sh
+++ download.sh
@@ -1,8 +1,19 @@
-USERNAME="admin"
-PASSWORD="mypassword"
-TIME=60 # seconds
-IP="192.168.1.197"
+# default parameters value
+TIME=60
 PORT=554
+OUTPUT_PATH="."
+
+while getopts u:p:i:t:n:o: flag
+do
+    case "${flag}" in
+        u) USERNAME=${OPTARG};;
+        p) PASSWORD=${OPTARG};;
+        i) IP=${OPTARG};;
+        t) TIME=${OPTARG};;
+        n) PORT=${OPTARG};;
+        o) OUTPUT_PATH=${OPTARG};;
+    esac
+done
 
 ffmpeg -i rtsp://$USERNAME:$PASSWORD@$IP:$PORT/h264Preview_01_main \
        -c copy \
@@ -12,5 +23,4 @@ ffmpeg -i rtsp://$USERNAME:$PASSWORD@$IP:$PORT/h264Preview_01_main \
        -segment_time $TIME \
        -strftime 1 \
        -segment_format mp4 \
-       "%Y-%m-%d--%H-%M-%S--capture-%04d.mp4"
-
+       "$OUTPUT_PATH/%Y-%m-%d--%H-%M-%S--capture-%04d.mp4"
